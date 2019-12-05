bc2c386ddc3b091062fae93d8ba6e384d05e6854
LangTran <58512089+LangTran@users.noreply.github.com>
2019-12-06 01:21:43 +1100

    Delete set-permissions.sh

--- Progs/set-permissions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# set-permissions.sh
-# makes langtran and langtran/Progs writable by group.
-#
-chgrp -R langtran /usr/local/langtran
-chmod g+ws /usr/local/langtran
-chmod g+ws /usr/local/langtran/Progs
